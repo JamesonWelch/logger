1a0d450f476cdda7e4d20f5d9bf945c7963e9893
Jameson <48740182+JamesonWelch@users.noreply.github.com>
2023-02-14 08:59:09 -0800

    basic config

---
+++ logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a formatter for the log messages
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+
+# Create a file handler for the log messages
+file_handler = logging.FileHandler('log.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+# Create a console handler for the log messages
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
